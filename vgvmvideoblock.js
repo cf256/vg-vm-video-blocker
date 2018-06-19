cc8b171e8173e95c4e3f2d50079657b5884685f1
cf256 <cflinchausen@gmail.com>
2018-06-19 22:42:34 +0200

    Fix typo in container class name

--- vgvmvideoblock.js
+++ vgvmvideoblock.js
@@ -1,7 +1,7 @@
-const VIDEO_CONTAIINER_CLASS_NAME = 'tv-container';
+const VIDEO_CONTAINER_CLASS_NAME = 'tv-container';
 
 const videoContainers = document.getElementsByClassName(
-  VIDEO_CONTAIINER_CLASS_NAME
+  VIDEO_CONTAINER_CLASS_NAME
 );
 
 Array.from(videoContainers).forEach(element => {
